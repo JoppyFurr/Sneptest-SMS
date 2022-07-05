21d0b76566ce836fd8df7bb1572f84d9077eb051
Joppy Furr <joppyfurr@gmail.com>
2022-07-05 22:58:06 +1200

    maint: Update build script, use c11

--- build.sh
+++ build.sh
@@ -3,29 +3,30 @@ echo ""
 echo "Sneptest SMS Build Script"
 echo "-------------------------"
 
-sdcc="${HOME}/Code/sdcc-4.1.0/bin/sdcc"
+CC="${HOME}/Code/sdcc-4.1.0/bin/sdcc"
 devkitSMS="${HOME}/Code/devkitSMS"
 SMSlib="${devkitSMS}/SMSlib"
 ihx2sms="${devkitSMS}/ihx2sms/Linux/ihx2sms"
 
+CFLAGS="--std-c11 -mz80 --peep-file ${devkitSMS}/SMSlib/src/peep-rules.txt -I ${SMSlib}/src"
+
 rm -r work
 mkdir -p work
 
-echo ""
-echo "Compiling..."
-for file in main
-do
-    echo " -> ${file}.c"
-    ${sdcc} -c -mz80 --peep-file ${devkitSMS}/SMSlib/src/peep-rules.txt -I ${SMSlib}/src \
-        -o "work/${file}.rel" "source/${file}.c" || exit 1
-done
+build_sneptest ()
+{
+    echo "Compiling..."
+    eval $CC $CFLAGS -c source/main.c -o work/main.rel || exit 1
 
-echo "Linking..."
-${sdcc} -o work/sneptest.ihx -mz80 --no-std-crt0 --data-loc 0xC000 ${devkitSMS}/crt0/crt0_sms.rel work/*.rel ${SMSlib}/SMSlib.lib || exit 1
+    echo "Linking..."
+    eval $CC -o work/sneptest.ihx -mz80 --no-std-crt0 --data-loc 0xC000 ${devkitSMS}/crt0/crt0_sms.rel work/*.rel ${SMSlib}/SMSlib.lib || exit 1
 
-echo ""
-echo "Generating ROM..."
-${ihx2sms} work/sneptest.ihx sneptest.sms || exit 1
+    echo "Generating ROM..."
+    eval $ihx2sms work/sneptest.ihx sneptest.sms || exit 1
 
-echo ""
-echo "Done"
+    echo "Done"
+}
+
+build_sneptest
+
+
